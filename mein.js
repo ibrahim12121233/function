e111102760cf53242c8b8dd9b54fc1eb434326d0
ibrahim <nsism99@gmail.com>
2023-08-21 11:36:53 +0300

    done

--- mein.js
+++ mein.js
@@ -3,3 +3,12 @@ function javascript_Boot_camp(teach1,tech2) {
     console.log("This javaScript BootCamp with " + javascript_Boot_camp +"OK");
 }
 javascript_Boot_camp("Teacher1", "Teacher2" )
+
+
+
+ let add = (a,b=c)=>{
+     return a + b;
+    
+  
+ };
+ console.log(add(5,3/2));
